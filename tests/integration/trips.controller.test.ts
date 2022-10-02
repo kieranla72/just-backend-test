d567cdb766615cee8997230383fcc28bc39cad0e
kieran lan g <kieran.lang@acin.com>
2022-10-02 14:59:52 +0100

    debuggingTests

--- tests/integration/trips.controller.test.ts
+++ tests/integration/trips.controller.test.ts
@@ -15,6 +15,11 @@ describe('Integration tests for the trips routes', () => {
     it('get - success - Gets all of a user trips', async () => {
       const { body, statusCode } = await request(app).get('/users/100/trips');
 
+      console.log('env variables============================\n');
+      console.log(process.env.POLICY_URL);
+      console.log(process.env.PUSH_URL);
+      console.log(process.env.DB_LOCATION);
+
       expect(body).toEqual(
         expect.arrayContaining([
           expect.objectContaining({
